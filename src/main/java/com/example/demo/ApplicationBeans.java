5af9cac89cdaec0dbba30295ac2273a41815ecfa
Vadi <nirmal.vadi@accenture.com>
2022-09-05 03:05:53 +0100

    updated

---
+++ src/main/java/com/example/demo/ApplicationBeans.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class ApplicationBeans {
+
+   @Autowired
+   public ApplicationBeans(Environment environment) {
+      
+      String property = "java.security.auth.login.config";
+      System.getProperties().setProperty(property, "/etc/jaas.conf");
+      
+      String property1 = "sun.security.krb5.debug";
+      System.getProperties().setProperty(property1, "true");
+      
+      String property2 = "javax.security.auth.useSubjectCredsOnly";
+      System.getProperties().setProperty(property2, "true");
+      
+   }
+
+}
